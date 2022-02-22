5f563a166baad9982fc26a3bc45039f36d89a3de
Willian Ribeiro <willian.ribeiro@buser.com.br>
2022-02-22 11:06:33 -0300

    Capítulo 2: implementando o modelo

---
+++ client/app/domain/negociacao/Negociacao.js
@@ -0,0 +1,24 @@
+class Negociacao {
+    constructor (_data, _quantidade, _valor) {
+        // Negociacao precisa ser imutável
+        this._data = new Date(_data.getTime())
+        Object.assign(this, { _quantidade, _valor })
+        Object.freeze(this)
+    }
+
+    get data () {
+        return this._data
+    }
+
+    get quantidade () {
+        return this._quantidade
+    }
+
+    get valor () {
+        return this._valor
+    }
+
+    get volume () {
+        return this._quantidade * this._valor
+    }
+}
