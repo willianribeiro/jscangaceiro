6a5e0b7969549dc3fcc12d8e76203e0c05fd03e3
Willian Ribeiro <willian.ribeiro@buser.com.br>
2022-03-10 11:00:33 -0300

    Capitulo 13: Isola responsabilidade de requisição assíncrona em um serviço (HttpService)

--- client/app/domain/negociacao/NegociacaoService.js
+++ client/app/domain/negociacao/NegociacaoService.js
@@ -1,28 +1,22 @@
 class NegociacaoService {
+    constructor() {
+        this._http = new HttpService()
+    }
+
     obterNegociacoesDaSemana() {
-        return new Promise((resolve, reject) => {
-            const xhr = new XMLHttpRequest()
-
-            xhr.open('GET', '/negociacoes/semana')
-
-            xhr.onreadystatechange = () => {
-                if (xhr.readyState === 4) {
-                    if (xhr.status === 200) {
-                        const negociacoes = JSON
-                            .parse(xhr.responseText)
-                            .map(negociacao => new Negociacao(
-                                new Date(negociacao.data),
-                                negociacao.quantidade,
-                                negociacao.valor
-                            ))
-                        resolve(negociacoes)
-                    } else {
-                        reject('Não foi possível obter nas negociações da semana')
-                    }
+        return this._http.get('/negociacoes/semana')
+            .then(
+                negociacoes => {
+                    return negociacoes
+                        .map(negociacao => new Negociacao(
+                            new Date(negociacao.data),
+                            negociacao.quantidade,
+                            negociacao.valor
+                        ))
+                },
+                error => {
+                    throw new Error('Não foi possível obter as negociações da semana')
                 }
-            }
-
-            xhr.send()
-        })
+            )
     }
 }
